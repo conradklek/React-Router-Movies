0da6eee1d5baebc0df51c3a2946af9b7dfad27c6
GooieJS <conradklek@gmail.com>
2022-02-21 22:54:03 -0600

    submit

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 export default function MovieList(props) {
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
+      {props.movies.map((movie) => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
@@ -12,10 +13,12 @@ export default function MovieList(props) {
 
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
-
+  //When a user clicks on the movie card inside MovieList they should be taken to /movies/{id of clicked movie here} to see the details of the selected movie.
   return (
     <div className="movie-card">
-      <h2>{title}</h2>
+      <h2>
+        <Link to={`/movies/${props.movie.id}`}>{title}</Link>
+      </h2>
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,20 @@
-import React from 'react';
+// make home button work
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export default function SavedList(props) {
   return (
-    <div className="saved-list">
-      <h3>Saved Movies:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
-      ))}
-      <div className="home-button">Home</div>
-    </div>
+    <Router>
+      <div className="saved-list">
+        <h3>Saved Movies:</h3>
+        {props.list.map((movie) => (
+          <span className="saved-movie">{movie.title}</span>
+        ))}
+        <div className="home-button">
+          <Link to="/">Home</Link>
+        </div>
+      </div>
+    </Router>
   );
 }
